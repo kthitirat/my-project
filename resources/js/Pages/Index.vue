ad2ed3ca65ca5e23f0729b3acd387bc88673a893
kthitirat <kthitirat@aru.ac.th>
2024-06-03 14:57:46 +0700

    feat: add mock index page

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,10 +1,26 @@
 <template>
     <Layout>
-        <div>ldlddfdfdfdffddfdfld</div>
-        <div>ldlddfdfdfdffddfdfld</div>
-        <div>ldlddfdfdfdffddfdfld</div>
-        <div>ldlddfdfdfdffddfdfld</div>
-        <div>ldlddfdfdfdffddfdfld</div>
+        <div class="w-full">
+            <div class="flex justify-center mt-5"> <!-- flex ให้อยู่บรรทัดเดียวกัน -->
+                <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-700">เอกสารประกอบการสอน</h1>
+                <h1 class="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-blue-600"> (PowerPoint)</h1>
+            </div>
+           <div class="grid grid-cols-4 gap-8 px-16 mt-10">
+                <div class="w-full border border-slate-200 p-4">
+                    <img class="w-full h-80 object-cover " src="https://picsum.photos/400/800" >
+                    <div class="mt-2">
+                        <p class="text-lg font-bold text-gray-700 text-center leading-5">รายวิชา GE64303 การออมและการลงทุน</p>
+                        <div class="text-sm text-center mt-4">
+                            <p>อาจารย์ ดร.มรุต กลัดเจริญ</p>
+                            <p>คณะวิทยาการจัดการ</p>
+                            <p>เผยแพร่วันที่ 1 มีนาคม 2567</p>
+                        </div>
+
+                    </div>
+                </div>
+
+           </div>
+        </div>
     </Layout>
 </template>
 
