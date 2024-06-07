4589f037abf1309a944da641dca355712774e7ae
kthitirat <kthitirat@aru.ac.th>
2024-06-07 16:55:22 +0700

    feat: add auto gen uuid when create

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -9,6 +9,7 @@ use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Illuminate\Support\Str;
 
 class Subject extends Model implements HasMedia
 {
@@ -34,6 +35,14 @@ class Subject extends Model implements HasMedia
         'published_at' => 'datetime',
         'view' => 'integer',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($subject) {
+            $subject->uuid = Str::uuid();
+        });
+    }
 
     //รูปภาพหน้าปก
     public function registerMediaCollections(): void
